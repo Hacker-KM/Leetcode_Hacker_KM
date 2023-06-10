5a7b0c4846e0d462b2df988104436f5ca58c65ee
Hacker-KM <122519511+Hacker-KM@users.noreply.github.com>
2023-06-11 02:18:23 +0530

    Time: 121 ms (16.50%), Space: 19.1 MB (5.06%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -1,11 +1,11 @@
 class Solution:
     def sumOddLengthSubarrays(self, l: List[int]) -> int:
         comb = [[]]
-        ans= sum(l)
+        ans= 0
         for i in range(len(l) + 1):
             for j in range(i):
                 comb.append(l[j: i])
         for i in comb:
-            if len(i)%2!=0 and len(i)>1:
+            if len(i)%2!=0:
                 ans= ans+sum(i)
         return ans
