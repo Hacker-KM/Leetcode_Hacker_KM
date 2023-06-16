a677a33ab06c8277084fa6562bf9de924159715b
Hacker-KM <122519511+Hacker-KM@users.noreply.github.com>
2023-06-17 02:35:20 +0530

    Time: 277 ms (27.96%), Space: 29.1 MB (6.98%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        res = 1000000000
+        left, total = 0, 0
+        
+        for i in range(len(nums)):
+            total += nums[i]
+            while total >= s:
+                res = min(res, i - left + 1)
+                total -= nums[left]
+                left += 1
+        
+        return res if res != 1000000000 else 0
