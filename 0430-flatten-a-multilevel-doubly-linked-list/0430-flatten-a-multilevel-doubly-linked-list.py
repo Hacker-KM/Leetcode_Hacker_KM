8de54ea9e1873041fea55bebaf1a16d3c209e959
Kartik Mishra <122519511+Hacker-KM@users.noreply.github.com>
2023-10-11 13:18:37 +0530

    Time: 40 ms (77.08%), Space: 16.8 MB (85.28%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
@@ -1,37 +1,27 @@
-# class Node:
-#     def __init__(self, val, prev=None, next=None, child=None):
-#         self.val = val
-#         self.prev = prev
-#         self.next = next
-#         self.child = child
+class Node:
+    def __init__(self, val, prev=None, next=None, child=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
 
 class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
         
-        def dfs(node):
-            current = node
-            last_node = None
-            
-            while current:
-                if current.child:
-                    next_node = current.next
-                    child_head, child_tail = dfs(current.child)
-                    current.next = child_head
-                    child_head.prev = current
-                    current.child = None
-                    
-                    if next_node:
-                        child_tail.next = next_node
-                        next_node.prev = child_tail
-                    
-                    last_node = child_tail
-                else:
-                    last_node = current
-                
-                current = current.next
-            
-            return node, last_node
-        head,curr = dfs(head)
+        current = head
+        while current:
+            if current.child:
+                next_node = current.next
+                child_head = current.child
+                while child_head.next:
+                    child_head = child_head.next
+                current.next = current.child
+                current.child.prev = current
+                current.child = None
+                child_head.next = next_node
+                if next_node:
+                    next_node.prev = child_head
+            current = current.next
         return head
