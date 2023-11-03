143f8f93515dbc551e10cddbcc84071593da0c06
Kartik Mishra <122519511+Hacker-KM@users.noreply.github.com>
2023-11-03 10:19:24 +0530

    Time: 42 ms (43.64%), Space: 16.3 MB (10.51%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -2,7 +2,6 @@ class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         target.sort()
         ans = []
-        stack = []
         j = 0
         for i in range(1,n+1):
             if j==len(target):
